a27461c6f3ec220eac00c446717149920e7ecd20
sjurske <george.america1993@gmail.com>
2024-02-21 14:31:48 +0100

    basic development

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -10,12 +10,12 @@ generate_password() {
 printf "${Green}\n---------------------Generating Passwords------------------------${Color_Off}\n"
 # Generate password for EMAIL_PASS
 E_PASS=$(generate_password)
-printf "Generated email password: ${UWhite}${E_PASS}${Color_Off}\n"
+printf "E-Mail Password: ${UWhite}${E_PASS}${Color_Off}\n"
 printf "${E_PASS}" > e_pw.md
 
 # Generate password for DB_PASS
 DB_PASS=$(generate_password)
-printf "Generated database password: ${UWhite}${DB_PASS}${Color_Off}\n"
+printf "Database Password: ${UWhite}${DB_PASS}${Color_Off}\n"
 printf "${DB_PASS}" > db_pw.md
 printf "Output stored in e_pw.md db_pw.md\n"
 printf "${BYellow}DELETE OR MOVE THEM TO A SECURE PATH\n"
