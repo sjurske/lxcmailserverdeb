26f8bc0840a0f6448a01eef92a97d764a5a8c90b
sjurske <george.america1993@gmail.com>
2024-02-21 15:11:28 +0100

    basic development

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 source ../misc/color.func
-# Update this list if out-of-date
 new_sources_list="
 deb http://deb.debian.org/debian bookworm main non-free-firmware
 deb-src http://deb.debian.org/debian bookworm main non-free-firmware
@@ -9,4 +8,6 @@ deb-src http://deb.debian.org/debian-security/ bookworm-security main non-free-f
 deb http://deb.debian.org/debian bookworm-updates main non-free-firmware
 deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware"
 echo "$new_sources_list" | sudo tee /etc/apt/sources.list > /dev/null
-apt update && apt full-upgrade -y && apt install -y net-tools mariadb-server mariadb-client postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql
+apt update
+apt full-upgrade -y
+apt install -y net-tools mariadb-server mariadb-client postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql
