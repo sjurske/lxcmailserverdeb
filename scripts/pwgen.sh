eb4908104bfeae07f13774b0ceb53528e0cb5e09
sjurske <george.america1993@gmail.com>
2024-02-21 14:30:56 +0100

    basic development

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -18,5 +18,5 @@ DB_PASS=$(generate_password)
 printf "Generated database password: ${UWhite}${DB_PASS}${Color_Off}\n"
 printf "${DB_PASS}" > db_pw.md
 printf "Output stored in e_pw.md db_pw.md\n"
-printf "${BYellow}DELETE OR MOVE THEM TO A SECURE PATH\n\n"
+printf "${BYellow}DELETE OR MOVE THEM TO A SECURE PATH\n"
 printf "${Green}-----------------Password Generation Completed-------------------${Color_Off}\n\n"
