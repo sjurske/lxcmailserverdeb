fcd316a28b6806419249d21bd583651ae74e6563
sjurske <george.america1993@gmail.com>
2024-02-26 14:52:23 +0100

    check device function

--- start.sh
+++ start.sh
@@ -131,7 +131,6 @@ main_menu() {
             *) printf "${Red}Invalid choice. Please enter correct value${Color_Off}\n\n" ;;
         esac
     else
-        printf "${BGreen}-----------------------OPTION--------------------------${Color_Off}\n"
         read -p "Do you want to install a Debian LXC on this host? (Y/N): " install_lxc_choice
         if [[ "$install_lxc_choice" =~ ^[Yy]$ ]]; then
             install_proxmox_lxc
