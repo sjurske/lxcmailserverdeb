1abb33ed0aea277c2b03b2b96573dd630e8ea177
sjurske <george.america1993@gmail.com>
2024-02-26 15:34:40 +0100

    sql conf fix

--- scripts/mailserver.sh
+++ scripts/mailserver.sh
@@ -17,8 +17,8 @@ USE $DATABASE;
 CREATE TABLE IF NOT EXISTS virtual_domains (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(50) NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS virtual_users (id INT NOT NULL AUTO_INCREMENT,domain_id INT NOT NULL,password VARCHAR(106) NOT NULL,email VARCHAR(120) NOT NULL,PRIMARY KEY (id),UNIQUE KEY email (email),FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS virtual_aliases (id INT NOT NULL AUTO_INCREMENT,domain_id INT NOT NULL,source varchar(100) NOT NULL,destination varchar(100) NOT NULL,PRIMARY KEY (id),FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO mailserver.virtual_domains(id, name) VALUES('1', '$DOMAIN'),('2', 'mail.$DOMAIN');
-INSERT INTO mailserver.virtual_users(id, domain_id, password, email)VALUES('1', '1', ENCRYPT('$E_PASS', CONCAT('\$6\$', SUBSTRING(SHA(RAND()), -16))), '$EMAIL');
+INSERT INTO $DATABASE.virtual_domains(id, name) VALUES('1', '$DOMAIN'),('2', 'mail.$DOMAIN');
+INSERT INTO $DATABASE.virtual_users(id, domain_id, password, email)VALUES('1', '1', ENCRYPT('$E_PASS', CONCAT('\$6\$', SUBSTRING(SHA(RAND()), -16))), '$EMAIL');
 EOF
 }
 bootstrapdb
