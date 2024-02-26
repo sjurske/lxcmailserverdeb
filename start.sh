5ab96691ae955ec11ac96663d476042e1afe4fb7
sjurske <george.america1993@gmail.com>
2024-02-26 10:04:02 +0100

    menu change

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+
 source misc/color.func
-source /etc/hostname
-printf "${BGreen}\n\n-----------------------SCRIPT MENU--------------------------\n\n${Color_Off}"
+
 check_root() {
     if [ "$(id -u)" != "0" ]; then
         printf "${Red}This script must be run as root${Color_Off}\n\n"
         exit 1
     fi
 }
-check_root
-printf "${Green}Script is running with root privileges${Color_Off}\n\n"
+
 validate_email() {
     local EMAIL=$1
     if [[ "$EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
@@ -18,6 +17,7 @@ validate_email() {
         return 1
     fi
 }
+
 validate_database() {
     local DATABASE=$1
     if [[ "$DATABASE" =~ ^[a-zA-Z0-9_]+$ ]]; then
@@ -26,6 +26,7 @@ validate_database() {
         return 1
     fi
 }
+
 validate_db_user() {
     local DB_USER=$1
     if [[ "$DB_USER" =~ ^[a-zA-Z0-9_]+$ ]]; then
@@ -34,6 +35,7 @@ validate_db_user() {
         return 1
     fi
 }
+
 validate_yes_no() {
     local input=$1
     if [[ "$input" =~ ^[YyNn]$ ]]; then
@@ -42,50 +44,39 @@ validate_yes_no() {
         return 1
     fi
 }
-read -p "Enter Domain Name: " DOMAIN
-echo "$DOMAIN" | tee /etc/hostname
-hostnamectl set-hostname "$DOMAIN"
-read -p "Enter E-Mail Address: " EMAIL
-while ! validate_email "$EMAIL"; do
-    read -p "Invalid input. Please enter a valid E-Mail Address: " EMAIL
-done
-read -p "Enter Database Name: " DATABASE
-while ! validate_database "$DATABASE"; do
-    read -p "Invalid input. Please enter a valid Database Name: " DATABASE
-done
-read -p "Enter Database Username: " DB_USER
-while ! validate_db_user "$DB_USER"; do
-    read -p "Invalid input. Please enter a valid Database Username: " DB_USER
-done
-printf "\n\n----------Overview of settings----------\n\n"
-printf "${Yellow}Hostname: $DOMAIN\n"
-printf "Domain: $DOMAIN\n"
-printf "E-Mail Address: $EMAIL\n"
-printf "Database Name: $DATABASE\n"
-printf "Database User Name: $DB_USER\n\n${Color_Off}"
-read -p "Are these settings correct? (Y/N): " settings_correct
-while ! validate_yes_no "$settings_correct"; do
-    read -p "Invalid input. Please enter Y or N: " settings_correct
-done
-if [[ "$settings_correct" =~ ^[Yy]$ ]]; then
-    printf "Great\n"
-    printf "Script will now generate corresponding passwords...\n\n"
-    bash scripts/pwgen.sh
-else
-    # If the settings are not correct, prompt the user to input the values again
-    if [[ "$settings_correct" =~ ^[Nn]$ ]]; then
-        # Restart from the beginning
-        echo "Please input the settings again."
-        # You may add code here to reset the hostname if needed
-        # If needed, the script could also be modified to store the entered values to avoid re-entering
-        continue
-    fi
-fi
-read -p "Do you want to install continue installing mailserver? (Y/N): " install_mailserver
-while ! validate_yes_no "$install_mailserver"; do
-    read -p "Invalid input. Please enter Y or N: " install_mailserver
-done
-if [[ "$install_mailserver" =~ ^[Yy]$ ]]; then
+
+install_proxmox_lxc() {
+    printf "Running Proxmox LXC Debian installation script...\n"
+    bash scripts/debian.sh
+}
+
+install_mailserver() {
+    printf "Running mailserver installation script...\n"
     bash scripts/deps.sh
     bash scripts/mailserver.sh
+}
+
+main_menu() {
+    printf "${BGreen}\n\n-----------------------SCRIPT MENU--------------------------\n\n${Color_Off}"
+    printf "1. Install Proxmox LXC Debian\n"
+    printf "2. Install Mailserver in a Debian machine\n\n"
+    read -p "Enter your choice (1 or 2): " choice
+    case $choice in
+        1) install_proxmox_lxc ;;
+        2) install_mailserver ;;
+        *) printf "${Red}Invalid choice. Please enter 1 or 2.${Color_Off}\n\n" ;;
+    esac
+}
+
+check_root
+printf "${Green}Script is running with root privileges${Color_Off}\n\n"
+
+if [[ "$1" == "--proxmox" ]]; then
+    read -p "Do you want to install a Debian LXC Container? (Y/N): " lxc_container
+    if [[ "$lxc_container" =~ ^[Yy]$ ]]; then
+        install_proxmox_lxc
+        exit 0
+    fi
 fi
+
+main_menu
