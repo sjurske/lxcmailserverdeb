92f5b1e6a156bc365826df398afd367f1e992d93
sjurske <george.america1993@gmail.com>
2024-02-27 16:18:09 +0100

    cleanup code

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -15,6 +15,5 @@ printf "${DB_PASS}" > db_pw.md
 printf "${White}--------------PASSWORDS GENERETED---------------${Color_Off}\n\n"
 printf "${UWhite}Stored in 'e_pw.md' 'db_pw.md'\n${Color_Off}"
 printf "${BYellow}DELETE OR MOVE THEM TO A SECURE PATH\n${Color_Off}\n"
-
 printf "${Green} - PRESS ENTER TO CONTINUE: ${Color_Off}\n"
 read -p ""
