0f68810bfc6fafb80273a0945061c37b7f414256
sjurske <george.america1993@gmail.com>
2024-02-27 14:54:31 +0100

    removed old files

--- misc/mailserver_deps.sh
+++ misc/mailserver_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source misc/color.func
-apt install -y mariadb-server mariadb-client postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql
+apt install -y mariadb-server mariadb-client postfix postfix-mysql policycoreutils-python-utils dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql
 printf "\n\n${BGreen}----SOFTWARE INSTALLED AND UPDATED----${Color_Off}\n\n"
--- misc/main_deps.sh
+++ misc/main_deps.sh
@@ -3,5 +3,5 @@ source misc/color.func
 bash misc/update_sources.sh
 apt update
 apt full-upgrade -y
-apt install -y htop net-tools python3 certbot
+apt install -y htop net-tools python3 certbot openssl
 printf "\n\n${BGreen}----SOURCES-UPDATED----${Color_Off}\n\n"
