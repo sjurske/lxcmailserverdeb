7fc6c457929f3eed5bba62e1fc23898d2e61597f
sjurske <george.america1993@gmail.com>
2024-02-27 16:00:51 +0100

    cleanup code

--- misc/main_deps.sh
+++ misc/main_deps.sh
@@ -4,4 +4,4 @@ bash misc/update_sources.sh
 apt update
 apt full-upgrade -y
 apt install -y htop net-tools python3 certbot openssl
-printf "\n\n${BGreen}----SOURCES-UPDATED----${Color_Off}\n\n"
+printf "${BGreen}----------------SOURCES-UPDATED-----------------${Color_Off}\n"
