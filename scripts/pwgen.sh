32f329d1b62d7ccb50fbed163679a0ce55e0c2c9
sjurske <george.america1993@gmail.com>
2024-02-27 16:12:28 +0100

    cleanup code

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -5,7 +5,7 @@ generate_password() {
     local password=$(openssl rand -base64 24)
     echo "$password"
 }
-printf "\n${BGreen}-------------GENERATING PASSWORDS---------------${Color_Off}\n\n"
+printf "\n${UWhite}-------------GENERATING PASSWORDS---------------${Color_Off}\n\n"
 E_PASS=$(generate_password)
 printf "E-Mail Password: ${UWhite}${E_PASS}${Color_Off}\n"
 printf "${E_PASS}" > e_pw.md
@@ -14,6 +14,6 @@ printf "Database Password: ${UWhite}${DB_PASS}${Color_Off}\n"
 printf "${DB_PASS}" > db_pw.md
 printf "${Cyan}Stored in 'e_pw.md' 'db_pw.md'\n"
 printf "${BYellow}DELETE OR MOVE THEM TO A SECURE PATH\n"
-printf "\n${BGreen}--------------PASSWORDS GENERETED---------------${Color_Off}\n\n"
+printf "\n${UWhite}--------------PASSWORDS GENERETED---------------${Color_Off}\n\n"
 printf "${Green} - PRESS ENTER TO CONTINUE: ${Color_Off}\n"
 read -p ""
