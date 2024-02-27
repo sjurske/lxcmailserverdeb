97079d0fbde8b7303c59b4a0bc0a1950420397d0
sjurske <george.america1993@gmail.com>
2024-02-27 16:03:00 +0100

    cleanup code

--- misc/webserver_deps.sh
+++ misc/webserver_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source misc/color.func
 apt install -y nginx python3-certbot-nginx php php-fpm
-printf "\n\n${BGreen}----SOFTWARE INSTALLED AND UPDATED----${Color_Off}\n\n"
+printf "${BGreen}---------SOFTWARE INSTALLED AND UPDATED---------${Color_Off}\n"
