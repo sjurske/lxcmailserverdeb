76db1994f5c062ed89412c28968296667bdc3afe
sjurske <55877906+sjurske@users.noreply.github.com>
2024-10-01 16:07:30 +0200

    Removed sysvinit-core package
    Removed on sysvinit and relying on systemd

--- misc/main_deps.sh
+++ misc/main_deps.sh
@@ -3,5 +3,5 @@ source misc/color.func
 bash misc/update_sources.sh
 apt update
 apt full-upgrade -y
-apt install -y htop net-tools python3 certbot openssl sysvinit-core
+apt install -y htop net-tools python3 certbot openssl
 printf "${BGreen}----------------SOURCES-UPDATED-----------------${Color_Off}\n"
